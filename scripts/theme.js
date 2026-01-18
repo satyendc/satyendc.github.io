df862b48c8ca7c47b800ccb1d156103494a1b9f6
satyendc <satyen.dc@gmail.com>
2026-01-17 21:55:42 -0600

    Delete scripts/theme.js

--- scripts/theme.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("themeToggle");
-
-toggle.onclick = () => {
-  const theme = document.body.dataset.theme;
-  document.body.dataset.theme = theme === "dark" ? "light" : "dark";
-};
