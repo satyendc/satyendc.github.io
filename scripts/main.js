8ede3db0e9308ea0e72b231d4aeb97e248ba5d47
satyendc <satyen.dc@gmail.com>
2026-01-17 21:54:42 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,5 @@
-const items = document.querySelectorAll(".timeline-item");
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
+document.querySelectorAll(".toggle").forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.classList.toggle("active");
   });
-}, { threshold: 0.2 });
-
-items.forEach(item => observer.observe(item));
+});
